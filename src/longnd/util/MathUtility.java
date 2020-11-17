eb450815857a6daa967134ba96404706896e2144
longdaungo <zedvidieu@gmail.com>
2020-11-17 08:04:58 +0700

    1st upload

---
+++ src/longnd/util/MathUtility.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longnd.util;
+
+/**
+ *
+ * @author IT
+ */
+public class MathUtility {
+    
+    //class nay chua cac ham tien ich dung chung cho moi noi 
+    //do do no phai nen la static
+    public static final double PI = 3.1415;
+    
+    //ham tinh n giai thua ,n >= 0 && n<= 20
+    //neu qua 20 tran kieu long
+    //0! = 1; 1!= 1;n! = 1,2,3...n
+   
+    public static long getFactorial(int n){
+        if(n< 0 || n> 20){
+            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+        // dua dau vao ca chon, mminh kong them tra ve 1 gia am chi sai roi
+        // mình chửi luôn
+        }
+        if(n == 0 | n ==1)
+            return 1;
+        //song sot den day, thi n >1 va <= 20 roi
+        long result =1;
+        for (int i = 2; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/se1418/math/util/Se1418MathUtil.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se1418.math.util;
+
+import longnd.util.MathUtility;
+
+/**
+ *
+ * @author IT
+ */
+public class Se1418MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //2. test hàm = mắt
+        System.out.println("5!:"+MathUtility.getFactorial(5));
+        // expected:120, actual:
+        // tao hi vọng mày ói về 120 khi tao gọi mày hàm giai thừa
+        
+        // và tést bộ dâta khác, test case, tihf huống khác
+        System.out.println("0:"+MathUtility.getFactorial(0));
+        //e:1, a:1 -> okie
+        
+        //e:ném về ngoại lệ IllegalArgumentException nếu gia thừa -5
+        System.out.println("-5!:"+MathUtility.getFactorial(-5));
+    }
+    
+}
+      // TODO code application logic here
+        //Code viet ra thi dev phai test truoc cai da
+        //có vài hình thức test Devlopment
+        //viết dàn khung của hàm getFactorial(int n)
+        // chạy thửu coi xanh đỉ, đỏ nghĩa là lỗi, xanh  nghĩa là ôn
+        // cứ liên tục, xanh đỏ như thế trong quá trình viết code
+// à sửa code, người ta gọi là lập trình hướng theo thỏa mãn
+// việc tét, TDD
+//2. sout(hàm d=kèm data) đểcoi kết quả ra sao , có đúng như mình kì vọng hay không
+// nó phải traer về như thế hay không -> xem kết quả test = mắt
+// 3JOPtion Pane của bên avaDesktop để popoup kết quả lên màn hình,vẫ xem = mắt
+//4. viết 1 trang web gọi hàm xử lí..., hao sức
+// ta chơi 1 và 2 cách
